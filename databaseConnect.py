f7b42ba3584f5b5fa28a33688cf7fbe588eedaf8
DS Deepanshu Samdani <39222636+deepanshusamdani@users.noreply.github.com>
2022-01-07 13:15:35 +0530

    updated to postgresql

--- databaseConnect.py
+++ databaseConnect.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-
-import mysql.connector as mysql
+import psycopg2
 
 def connectdb():
-    try:
-        conn = mysql.connect(user='sd',password='Deepu@123',database='samiti',host='127.0.0.1')
-        #check the connection with database
-        if conn.is_connected():
+    try: 
+        conn = psycopg2.connect(database='d3sp7emltr5ruq', 
+                                user='bhbfatufvucgzo', 
+                                password='b2f9d1762fa505f7d975ade20047694d7752f613b9948201153c5896fb88aae9', 
+                                host='ec2-34-239-196-254.compute-1.amazonaws.com', 
+                                port= '5432')
+        cursor = conn.cursor()
+        if conn is not None:
             print("Database Connection is established")
         return conn
     except:
